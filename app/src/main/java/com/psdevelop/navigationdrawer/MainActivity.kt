bd455ebf5a1d021c74f38e4d2f4500d33534dca0
PuspendraSahu <sahu1122sumit@gmail.com>
2022-09-08 12:00:05 +0530

    1.0.1

--- app/src/main/java/com/psdevelop/navigationdrawer/MainActivity.kt
+++ app/src/main/java/com/psdevelop/navigationdrawer/MainActivity.kt
@@ -9,18 +9,73 @@ import android.widget.Toast
 
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.navigation.NavigationView
-
+import java.util.ArrayList
 
 
 class MainActivity : AppCompatActivity() {
 
    private lateinit var toggle : ActionBarDrawerToggle
+    private lateinit var newRecyclerView : RecyclerView
+    private lateinit var newArrayList : ArrayList<news>
+    private lateinit var titleImage : Array<Int>
+    private lateinit var heading : Array<String>
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         title = "Home"
+
+        titleImage = arrayOf(
+            R.drawable.photo11,
+            R.drawable.aa,
+            R.drawable.b,
+            R.drawable.c,
+            R.drawable.d,
+            R.drawable.e,
+            R.drawable.f,
+            R.drawable.g,
+            R.drawable.h,
+            R.drawable.i,
+            R.drawable.k,
+
+
+
+
+
+
+
+            )
+        heading = arrayOf(
+
+            "I am very excited because this is my first recycler view app and i am trying to make it good",
+            "I am very excited because this is my first recycler view app and i am trying to make it good",
+            "I am very excited because this is my first recycler view app and i am trying to make it good",
+            "I am very excited because this is my first recycler view app and i am trying to make it good",
+            "I am very excited because this is my first recycler view app and i am trying to make it good",
+            "I am very excited because this is my first recycler view app and i am trying to make it good",
+            "I am very excited because this is my first recycler view app and i am trying to make it good",
+            "I am very excited because this is my first recycler view app and i am trying to make it good",
+            "I am very excited because this is my first recycler view app and i am trying to make it good",
+            "I am very excited because this is my first recycler view app and i am trying to make it good",
+            "I am very excited because this is my first recycler view app and i am trying to make it good",
+        )
+
+
+        newRecyclerView = findViewById(R.id.recyclerView)
+        newRecyclerView.layoutManager = LinearLayoutManager(this)
+        newRecyclerView.setHasFixedSize(true)
+
+
+
+
+        newArrayList = arrayListOf<news>()
+        getUserdata()
+
+
 
         val drawerLayout : DrawerLayout = findViewById(R.id.drawerLayout)
         val navView : NavigationView = findViewById(R.id.nav_view)
@@ -59,6 +114,30 @@ class MainActivity : AppCompatActivity() {
         }
 
         return super.onOptionsItemSelected(item)
+    }
+    private fun getUserdata() {
+
+        for (i in titleImage.indices){
+
+            val News = news(titleImage[i] , heading[i])
+            newArrayList.add(News)
+
+        }
+
+        var adaptor = MyAdaptor(newArrayList)
+        newRecyclerView.adapter = adaptor
+        adaptor.setOnItemClickListener((object : MyAdaptor.onItemClickLestener{
+            override fun onItemClick(position: Int) {
+                Toast.makeText(this@MainActivity, "You Click On Number $position", Toast.LENGTH_SHORT).show()
+            }
+
+
+        }))
+
+
+
+
+
     }
 
 }
